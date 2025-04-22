f6b805a2a91ec6692c566452d9ddd5732108fd71
javasoftarun <javasoftarun159@gmail.com>
2025-04-22 17:02:34 +0530

    get image by user id

--- src/main/java/com/yatranow/CommonService/controller/CloudinaryController.java
+++ src/main/java/com/yatranow/CommonService/controller/CloudinaryController.java
@@ -1,11 +1,18 @@
 package com.yatranow.CommonService.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.yatranow.CommonService.entity.UploadedImage;
+import com.yatranow.CommonService.repository.UploadedImageRepository;
 import com.yatranow.CommonService.request.ImageUploadRequest;
 import com.yatranow.CommonService.response.ApiResponse;
 import com.yatranow.CommonService.service.CloudinaryService;
@@ -15,6 +22,9 @@ import com.yatranow.CommonService.service.CloudinaryService;
 public class CloudinaryController {
 
     private final CloudinaryService cloudinaryService;
+    
+    @Autowired
+    private UploadedImageRepository uploadedImageRepository;
 
     public CloudinaryController(CloudinaryService cloudinaryService) {
         this.cloudinaryService = cloudinaryService;
@@ -35,5 +45,15 @@ public class CloudinaryController {
             return ResponseEntity.status(500).body(new ApiResponse("error", e.getMessage(), 500));
         }
     }
+    
+    @GetMapping("/getImage/{userId}")
+    public ResponseEntity<ApiResponse> getImage(@PathVariable("userId") String userId) {
+		try {
+			List<UploadedImage> response = uploadedImageRepository.getImageUrlByUserId(userId);
+			return ResponseEntity.ok(new ApiResponse("success", response, 200));
+		} catch (Exception e) {
+			return ResponseEntity.status(500).body(new ApiResponse(e.getMessage(), null, 500));
+		}
+	}
 }
 
--- src/main/java/com/yatranow/CommonService/repository/UploadedImageRepository.java
+++ src/main/java/com/yatranow/CommonService/repository/UploadedImageRepository.java
@@ -1,11 +1,19 @@
 package com.yatranow.CommonService.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.yatranow.CommonService.entity.UploadedImage;
 
 @Repository
 public interface UploadedImageRepository extends JpaRepository<UploadedImage, Long> {
+
+	@Query("SELECT u FROM UploadedImage u WHERE u.userId = :userId")
+	List<UploadedImage> getImageUrlByUserId(@Param("userId") String userId);
+
 }
 
