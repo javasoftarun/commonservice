c5f8683360da0422e843c3d4d7c9ca40e890c250
javasoftarun <javasoftarun159@gmail.com>
2025-04-26 15:39:16 +0530

    cors enable

--- src/main/java/com/yatranow/CommonService/controller/CabRatingController.java
+++ src/main/java/com/yatranow/CommonService/controller/CabRatingController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,6 +20,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/api/common/cab-rating/")
 @Tag(name = "Cab Rating API", description = "API for managing cab ratings")
