a21d3df8b9b673db9a1c625a8bed570f039cd56a
javasoftarun <javasoftarun159@gmail.com>
2025-06-01 08:28:55 +0530

    change offer fields

--- src/main/java/com/yatranow/CommonService/entity/Offer.java
+++ src/main/java/com/yatranow/CommonService/entity/Offer.java
@@ -9,86 +9,83 @@ public class Offer {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String title;
+    private String promocode;
+    private String description;
+    private double discount;
+    private double minFare;
+    private double discountPercentage;
+    private double maxDiscount;
     private String state;
     private String city;
-    private Double percentage;
-    private String promocode;
-    private String restriction;
     private LocalDateTime promoStartDate;
     private LocalDateTime promoEndDate;
+    
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getPromocode() {
+		return promocode;
+	}
+	public void setPromocode(String promocode) {
+		this.promocode = promocode;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public double getDiscount() {
+		return discount;
+	}
+	public void setDiscount(double discount) {
+		this.discount = discount;
+	}
+	public double getMinFare() {
+		return minFare;
+	}
+	public void setMinFare(double minFare) {
+		this.minFare = minFare;
+	}
+	public double getDiscountPercentage() {
+		return discountPercentage;
+	}
+	public void setDiscountPercentage(double discountPercentage) {
+		this.discountPercentage = discountPercentage;
+	}
+	public double getMaxDiscount() {
+		return maxDiscount;
+	}
+	public void setMaxDiscount(double maxDiscount) {
+		this.maxDiscount = maxDiscount;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public LocalDateTime getPromoStartDate() {
+		return promoStartDate;
+	}
+	public void setPromoStartDate(LocalDateTime promoStartDate) {
+		this.promoStartDate = promoStartDate;
+	}
+	public LocalDateTime getPromoEndDate() {
+		return promoEndDate;
+	}
+	public void setPromoEndDate(LocalDateTime promoEndDate) {
+		this.promoEndDate = promoEndDate;
+	}
 
-    // Getters and Setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public Double getPercentage() {
-        return percentage;
-    }
-
-    public void setPercentage(Double percentage) {
-        this.percentage = percentage;
-    }
-
-    public String getPromocode() {
-        return promocode;
-    }
-
-    public void setPromocode(String promocode) {
-        this.promocode = promocode;
-    }
-
-    public String getRestriction() {
-        return restriction;
-    }
-
-    public void setRestriction(String restriction) {
-        this.restriction = restriction;
-    }
-
-    public LocalDateTime getPromoStartDate() {
-        return promoStartDate;
-    }
-
-    public void setPromoStartDate(LocalDateTime promoStartDate) {
-        this.promoStartDate = promoStartDate;
-    }
-
-    public LocalDateTime getPromoEndDate() {
-        return promoEndDate;
-    }
-
-    public void setPromoEndDate(LocalDateTime promoEndDate) {
-        this.promoEndDate = promoEndDate;
-    }
 }
 
